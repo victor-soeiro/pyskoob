b81e18c71cdb6637bc3e8bbcbe35ccfd57478046
Victor Soeiro <victor.soeiro.araujo@gmail.com>
2025-07-30 22:45:32 -0300

    Remove comments from tests

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,10 +1,11 @@
 import importlib
 
 modules = [
-    'app.api.routers.books',
-    'app.api.routers.profile_actions',
-    'app.api.routers.users',
-    'app.models.author',
+    'pyskoob.auth',
+    'pyskoob.books',
+    'pyskoob.users',
+    'pyskoob.profile',
+    'pyskoob.models.author',
 ]
 
 for mod in modules:
