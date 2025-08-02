235d0a52d1c24857f97ecf43695475b0a12901dc
Victor Soeiro <victor.soeiro.araujo@gmail.com>
2025-08-02 00:06:13 -0300

    fix: validate change_book_shelf response

--- pyskoob/profile.py
+++ pyskoob/profile.py
@@ -160,7 +160,7 @@ class SkoobProfileService(BaseSkoobService):
         Returns
         -------
         bool
-            True if the bookshelf was changed successfully.
+            True if the bookshelf was changed successfully, False otherwise.
 
         Examples
         --------
@@ -171,7 +171,7 @@ class SkoobProfileService(BaseSkoobService):
         url = f"{self.base_url}/estante/prateleira/{edition_id}/{bookshelf.value}"
         response = self.client.get(url)
         response.raise_for_status()
-        return True
+        return response.json().get("success", False)
 
     def rate_book(self, edition_id: int, ranking: float) -> bool:
         """
