16d0dc43c0d6a8b3e59311f6dee894ad75f96da6
Victor Soeiro <victor.soeiro.araujo@gmail.com>
2025-08-03 12:16:20 -0300

    style: format Skoob parser tests

--- tests/test_skoob_parser_utils.py
+++ tests/test_skoob_parser_utils.py
@@ -14,15 +14,9 @@ def test_get_book_edition_and_user_id():
     assert spu.get_book_edition_id_from_url(url) == "456"
     url_with_query = "https://www.skoob.com.br/livro/123-ed456.html?foo=bar"
     assert spu.get_book_edition_id_from_url(url_with_query) == "456"
-    assert (
-        spu.get_user_id_from_url("https://www.skoob.com.br/usuario/55-name/?x=y")
-        == "55"
-    )
+    assert spu.get_user_id_from_url("https://www.skoob.com.br/usuario/55-name/?x=y") == "55"
 
 
 def test_get_author_id_from_url():
     assert spu.get_author_id_from_url("https://www.skoob.com.br/autor/77-john") == "77"
-    assert (
-        spu.get_author_id_from_url("https://www.skoob.com.br/autor/77-john/?ref=bar")
-        == "77"
-    )
+    assert spu.get_author_id_from_url("https://www.skoob.com.br/autor/77-john/?ref=bar") == "77"
